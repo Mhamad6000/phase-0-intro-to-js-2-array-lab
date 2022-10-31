6a4fc3e868214e28a9225a7234e1b93ed9c6e208
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-31 17:57:38 +0300

    done but it kepps giving me eroor ?

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(name) {
+  // im doing what is written in test and it gives me error for no reason , im returning new array leaving the cats array unchanged
+  const newArr = cats;
+  newArr.push(name);
+  return newArr;
+}
+function prependCat(name) {
+  const newArr = cats;
+  newArr.unshift(name);
+  return newArr;
+}
+function removeLastCat() {
+  const newArr = cats.pop();
+  return newArr;
+}
+function removeFirstCat() {
+  const newArr = cats;
+  newArr.pop(0);
+  return newArr;
+}
