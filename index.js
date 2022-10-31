ce0c758e634917e08d170bd41b28f40ef29cf082
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-31 19:45:45 +0300

    full activity done

--- index.js
+++ index.js
@@ -14,21 +14,22 @@ function destructivelyRemoveFirstCat() {
 }
 function appendCat(name) {
   // im doing what is written in test and it gives me error for no reason , im returning new array leaving the cats array unchanged
-  const newArr = cats;
+  const newArr = [...cats];
   newArr.push(name);
   return newArr;
 }
 function prependCat(name) {
-  const newArr = cats;
+  const newArr = [...cats];
   newArr.unshift(name);
   return newArr;
 }
 function removeLastCat() {
-  const newArr = cats.pop();
+  const newArr = [...cats];
+  newArr.pop();
   return newArr;
 }
 function removeFirstCat() {
-  const newArr = cats;
-  newArr.pop(0);
+  const newArr = [...cats];
+  newArr.shift();
   return newArr;
 }
